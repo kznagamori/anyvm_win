c1ca87a2d41496ace172999f819fe453ed115d48
kznagamori <kazuyuki.nagamori@gmail.com>
2023-11-07 19:57:58 +0900

    Typo修正

--- bin/anyvm.bat
+++ bin/anyvm.bat
@@ -1,9 +1,8 @@
 @ECHO OFF
 
 IF "%~1"=="rehash" (
-    %~dp0..\scripts\AnyVmDeativate.bat
-    %~dp0..\scripts\AnyVmAativate.bat
+    %~dp0..\scripts\AnyVmDeactivate.bat
+    %~dp0..\scripts\AnyVmActivate.bat
     GOTO :EOF
 )
-
 %~dp0anyvm_win.exe %*
