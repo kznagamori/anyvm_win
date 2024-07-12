c2352d490f0b44955d75032087f53601ad455ff1
kznagamori <kazuyuki.nagamori@gmail.com>
2024-07-12 20:42:29 +0900

    すべてに対してのunset/updateを追加

--- bin/anyvm.bat
+++ bin/anyvm.bat
@@ -5,4 +5,38 @@ IF "%~1"=="rehash" (
     %~dp0..\scripts\AnyVmActivate.bat
     GOTO :EOF
 )
+IF "%~1"=="update" (
+    %~dp0anyvm_win.exe  BazelVm update     
+    %~dp0anyvm_win.exe  CMakeVm update     
+    %~dp0anyvm_win.exe  DartVm update      
+    %~dp0anyvm_win.exe  FlutterVm update   
+    %~dp0anyvm_win.exe  GoVm update        
+    %~dp0anyvm_win.exe  GradleVm update    
+    %~dp0anyvm_win.exe  JDKVm update       
+    %~dp0anyvm_win.exe  LLVMVm update      
+    %~dp0anyvm_win.exe  MinGWVm update     
+    %~dp0anyvm_win.exe  NinjaVm update     
+    %~dp0anyvm_win.exe  NodejsVm update    
+    %~dp0anyvm_win.exe  PythonVm update    
+    %~dp0anyvm_win.exe  dotnetVm update    
+    GOTO :EOF
+)
+IF "%~1"=="unset" (
+    %~dp0anyvm_win.exe  AndroidSDKVm unset
+    %~dp0anyvm_win.exe  BazelVm unset     
+    %~dp0anyvm_win.exe  CMakeVm unset     
+    %~dp0anyvm_win.exe  DartVm unset      
+    %~dp0anyvm_win.exe  FlutterVm unset   
+    %~dp0anyvm_win.exe  GoVm unset        
+    %~dp0anyvm_win.exe  GradleVm unset    
+    %~dp0anyvm_win.exe  JDKVm unset       
+    %~dp0anyvm_win.exe  LLVMVm unset      
+    %~dp0anyvm_win.exe  MinGWVm unset     
+    %~dp0anyvm_win.exe  NinjaVm unset     
+    %~dp0anyvm_win.exe  NodejsVm unset    
+    %~dp0anyvm_win.exe  PythonVm unset    
+    %~dp0anyvm_win.exe  RustVm unset      
+    %~dp0anyvm_win.exe  dotnetVm unset    
+    GOTO :EOF
+)
 %~dp0anyvm_win.exe %*
--- bin/anyvm.ps1
+++ bin/anyvm.ps1
@@ -1,17 +1,56 @@
 $firstArg = $args[0]
 
-function Perform-Rehash {
-    . $PSScriptRoot\..\scripts\AnyVmDeactivate.ps1
-    . $PSScriptRoot\..\scripts\AnyVmActivate.ps1
-}
-
 function Invoke-AnyVm {
     param($arguments)
     & "$PSScriptRoot\anyvm_win.exe" @arguments
 }
 
+function Perform-Rehash {
+    . $PSScriptRoot\..\scripts\AnyVmDeactivate.ps1
+    . $PSScriptRoot\..\scripts\AnyVmActivate.ps1
+}
+
+function Perform-Update {
+    & "$PSScriptRoot\anyvm_win.exe"  BazelVm update     
+    & "$PSScriptRoot\anyvm_win.exe"  CMakeVm update     
+    & "$PSScriptRoot\anyvm_win.exe"  DartVm update      
+    & "$PSScriptRoot\anyvm_win.exe"  FlutterVm update   
+    & "$PSScriptRoot\anyvm_win.exe"  GoVm update        
+    & "$PSScriptRoot\anyvm_win.exe"  GradleVm update    
+    & "$PSScriptRoot\anyvm_win.exe"  JDKVm update       
+    & "$PSScriptRoot\anyvm_win.exe"  LLVMVm update      
+    & "$PSScriptRoot\anyvm_win.exe"  MinGWVm update     
+    & "$PSScriptRoot\anyvm_win.exe"  NinjaVm update     
+    & "$PSScriptRoot\anyvm_win.exe"  NodejsVm update    
+    & "$PSScriptRoot\anyvm_win.exe"  PythonVm update    
+    & "$PSScriptRoot\anyvm_win.exe"  dotnetVm update    
+}
+
+function Perform-Unset {
+    & "$PSScriptRoot\anyvm_win.exe"  AndroidSDKVm unset
+    & "$PSScriptRoot\anyvm_win.exe"  BazelVm unset     
+    & "$PSScriptRoot\anyvm_win.exe"  CMakeVm unset     
+    & "$PSScriptRoot\anyvm_win.exe"  DartVm unset      
+    & "$PSScriptRoot\anyvm_win.exe"  FlutterVm unset   
+    & "$PSScriptRoot\anyvm_win.exe"  GoVm unset        
+    & "$PSScriptRoot\anyvm_win.exe"  GradleVm unset    
+    & "$PSScriptRoot\anyvm_win.exe"  JDKVm unset       
+    & "$PSScriptRoot\anyvm_win.exe"  LLVMVm unset      
+    & "$PSScriptRoot\anyvm_win.exe"  MinGWVm unset     
+    & "$PSScriptRoot\anyvm_win.exe"  NinjaVm unset     
+    & "$PSScriptRoot\anyvm_win.exe"  NodejsVm unset    
+    & "$PSScriptRoot\anyvm_win.exe"  PythonVm unset    
+    & "$PSScriptRoot\anyvm_win.exe"  RustVm unset      
+    & "$PSScriptRoot\anyvm_win.exe"  dotnetVm unset    
+}
+
+
 if ($firstArg -eq "rehash") {
     Perform-Rehash
+} elseif  ($firstArg -eq "update") {
+    Perform-Update
+} elseif  ($firstArg -eq "unset") {
+    Perform-Unset
 } else {
     Invoke-AnyVm -arguments $args
 }
